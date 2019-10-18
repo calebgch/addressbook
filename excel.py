a57a02c94452aeec3a731c37f3ea3b2a5b6aad63
calebgch <calebgch@163.com>
2019-10-18 10:32:35 +0800

    remove some useless code

--- excel.py
+++ excel.py
@@ -37,15 +37,3 @@ reader.read_sheet("股份公司")
 reader.write_file("d:\\names.json")
 
 
-
-#sheet4 = wb['SQL']
-#for sheet in wb:
-#    print(sheet.title)
-
-
-#print(ws['A1']) # 获取A列的第一个对象
-#print(ws['A1'].value)
-
-#c = ws['B1']
-#print('Row {}, Column {} is {}'.format(c.row, c.column, c.value)) # 打印这个单元格对象所在的行列的数值和内容
-#print('Cell {} is {}\n'.format(c.coordinate, c.value)) # 获取单元格对象的所在列的行数和值
